271fdf848187bb9dc135e710d8644dc19c0e6795
Muhammad Ali <90325088+Ali0092@users.noreply.github.com>
2025-05-29 02:51:53 +0500

    completed basic structuring

--- app/src/main/java/com/example/composecarousel/MainActivity.kt
+++ app/src/main/java/com/example/composecarousel/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.composecarousel.ui.theme.ComposeCarouselTheme
 import com.example.composecarousellib.ComposeCarouselSlider
 
@@ -20,7 +26,7 @@ class MainActivity : ComponentActivity() {
             ComposeCarouselTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android", modifier = Modifier.padding(innerPadding)
+                       modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -29,16 +35,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    ComposeCarouselSlider(
-        listOf(
-            R.drawable.image1,
-            R.drawable.image2,
-            R.drawable.image3,
-            R.drawable.image4,
-            R.drawable.image5,
-            R.drawable.image6
+fun Greeting(modifier: Modifier = Modifier) {
+
+    Column(modifier = modifier.fillMaxSize().background(Color(0xFFD1DAD5)), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        ComposeCarouselSlider(
+            modifier = Modifier.padding(vertical = 12.dp),
+            height = 600.dp,
+            sidePadding = 40.dp,
+            pageSpacing = 1.dp,
+            imageCornerRoundness = 5.dp,
+            listOf(
+                R.drawable.image1,
+                R.drawable.image2,
+                R.drawable.image3,
+                R.drawable.image4,
+                R.drawable.image5,
+                R.drawable.image6
+            )
         )
-    )
+    }
+
 }
 
--- composecarousellib/src/main/java/com/example/composecarousellib/ComposeCarouselSlider.kt
+++ composecarousellib/src/main/java/com/example/composecarousellib/ComposeCarouselSlider.kt
@@ -2,12 +2,10 @@ package com.example.composecarousellib
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
@@ -16,47 +14,45 @@ import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
 import coil.compose.rememberAsyncImagePainter
 import kotlin.math.absoluteValue
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.util.lerp
-
 
 @Composable
 fun ComposeCarouselSlider(
-    popularMoviesList: List<Int> = emptyList()
+    modifier: Modifier = Modifier,
+    height: Dp,
+    sidePadding: Dp,
+    pageSpacing: Dp = 8.dp,
+    imageCornerRoundness: Dp = 8.dp,
+    popularMoviesList: List<Int> = emptyList(),
 ) {
     val pagerState = rememberPagerState(pageCount = { popularMoviesList.size })
+    HorizontalPager(
+        modifier = modifier,
+        state = pagerState,
+        contentPadding = PaddingValues(horizontal = sidePadding),
+        verticalAlignment = Alignment.CenterVertically,
+        pageSpacing = pageSpacing
+    ) { page ->
+        Card(modifier = Modifier
+            .clickable {
 
-    Column(modifier = Modifier.fillMaxSize().padding(vertical = 100.dp)) {
-        HorizontalPager(
-            state = pagerState,
-            contentPadding = PaddingValues(horizontal = 50.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            pageSpacing = 8.dp
-        ) { page ->
-            //defined in ClubbedPhotos.kt.
-
-            Card(
-                modifier = Modifier
-                    .clickable {
-
-                    }
-                    .height(350.dp)
-                    .width(320.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .carouselTransition(page, pagerState), shape = RoundedCornerShape(15.dp)
-            ) {
-                Image(
-                    modifier = Modifier.fillMaxSize(),
-                    painter = rememberAsyncImagePainter(popularMoviesList[page]),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop
-                )
             }
+            .height(height)
+            .fillMaxWidth()
+            .carouselTransition(page, pagerState),
+            shape = RoundedCornerShape(imageCornerRoundness)) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = rememberAsyncImagePainter(popularMoviesList[page]),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
         }
     }
 
@@ -67,7 +63,7 @@ fun Modifier.carouselTransition(page: Int, pagerState: PagerState) = graphicsLay
         ((pagerState.currentPage - page) + pagerState.currentPageOffsetFraction).absoluteValue
 
     val transformation = lerp(
-        start = 0.80f, stop = 1f, fraction = 1f - pageOffset.coerceIn(0f, 1f)
+        start = 0.90f, stop = 1f, fraction = 1f - pageOffset.coerceIn(0f, 1f)
     )
     alpha = transformation
     scaleY = transformation

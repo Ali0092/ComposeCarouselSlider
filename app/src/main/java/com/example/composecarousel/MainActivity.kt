e5e58543f01c3244fe07e0df0366ba5f87f5fcbe
Muhammad Ali <90325088+Ali0092@users.noreply.github.com>
2025-06-04 01:01:05 +0500

    added autoScroller, and animations

--- app/src/main/java/com/example/composecarousel/MainActivity.kt
+++ app/src/main/java/com/example/composecarousel/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -38,25 +42,35 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
 
+    val dataList = listOf(
+        CarouselImage.Resource(R.drawable.image1),
+        CarouselImage.Resource(R.drawable.image2),
+        CarouselImage.Resource(R.drawable.image3),
+        CarouselImage.Resource(R.drawable.image4),
+        CarouselImage.Resource(R.drawable.image5)
+    )
+
     Column(modifier = modifier.fillMaxSize().background(Color.White), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
         ComposeCarouselSlider(
             modifier = Modifier.padding(vertical = 12.dp),
             height = 600.dp,
             sidePadding = 40.dp,
             pageSpacing = 1.dp,
-            imageCornerRoundness = 5.dp,
+            imageCornerRoundness = 25.dp,
             nonSelectedDotColor = Color.Gray,
             selectedDotColor = Color.DarkGray,
-            imagesList = listOf(
-                CarouselImage.Resource(R.drawable.image1),
-                CarouselImage.Resource(R.drawable.image2),
-                CarouselImage.Resource(R.drawable.image3),
-                CarouselImage.Resource(R.drawable.image4),
-                CarouselImage.Resource(R.drawable.image5)
-            )
-        ) { index ->
-
-        }
+            delay = 2000,
+            enableAutoScroll = true,
+            enableAnimationOnAutoScroll = true,
+            animationSpecs = spring(
+                dampingRatio = Spring.DampingRatioLowBouncy,
+                stiffness = Spring.StiffnessLow
+            ),
+            imagesList = dataList,
+            getOnClick =  { index ->
+                //index of clicked image
+            }
+        )
     }
 
 }
--- composecarousellib/src/main/java/com/example/composecarousellib/ComposeCarouselSlider.kt
+++ composecarousellib/src/main/java/com/example/composecarousellib/ComposeCarouselSlider.kt
@@ -1,6 +1,9 @@
 package com.example.composecarousellib
 
 import android.R.attr.bitmap
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -21,6 +24,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,6 +38,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
 import coil.compose.rememberAsyncImagePainter
+import kotlinx.coroutines.delay
 import kotlin.math.absoluteValue
 
 
@@ -49,9 +54,25 @@ fun ComposeCarouselSlider(
     useDotIndicator: Boolean = true,
     nonSelectedDotColor: Color = Color.Gray,
     selectedDotColor: Color = Color.White,
+    delay: Long = 3_000L,
+    enableAutoScroll: Boolean = false,
+    enableAnimationOnAutoScroll: Boolean = false,
+    animationSpecs: AnimationSpec<Float> = spring(),
     getOnClick: (Int) -> Unit =  {}
 ) {
     val pagerState = rememberPagerState(pageCount = { imagesList.size })
+
+    if (enableAutoScroll) {
+        LaunchedEffect(pagerState) {
+            while (true) {
+                delay(delay)
+                val nextPage = (pagerState.currentPage + 1) % pagerState.pageCount
+                pagerState.animateScrollToPage(nextPage, animationSpec = if (enableAnimationOnAutoScroll) {
+                    animationSpecs
+                }else spring())
+            }
+        }
+    }
 
     Column(modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)) {
         HorizontalPager(
@@ -105,8 +126,7 @@ fun CarouselImageItem(image: CarouselImage) {
 }
 
 fun Modifier.carouselTransition(page: Int, pagerState: PagerState) = graphicsLayer {
-    val pageOffset =
-        ((pagerState.currentPage - page) + pagerState.currentPageOffsetFraction).absoluteValue
+    val pageOffset = ((pagerState.currentPage - page) + pagerState.currentPageOffsetFraction).absoluteValue
 
     val transformation = lerp(
         start = 0.80f, stop = 1f, fraction = 1f - pageOffset.coerceIn(0f, 1f)
